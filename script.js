d62c7b67c0027cc7b6083c233b4f705e357d6e3a
oricardos <ricardo.rs95@gmail.com>
2025-12-19 14:05:28 -0300

    anotacao e inferencia de tipo

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
-//@ts-check
-var total = 100.05;
-console.log(total.toFixed());
+"use strict";
+//Annotation
+let produto = "Livro";
+let preco = 200;
+const carro = {
+    marca: "Toyota",
+    portas: 5
+};
+//Inference
+let produto2 = "Livro";
+let preco2 = 200;
+const carro2 = {
+    marca: "Toyota",
+    portas: 5
+};
+const barato = preco < 400 ? true : "Produto caro";
+//-------
+function somar(a, b) {
+    return a + b;
+}
+somar(3, 10);
+const nintendo = {
+    nome: "Switch",
+    preco: '2000',
+};
+function transformarPreco(produto) {
+    produto.preco = `R$${produto.preco},00`;
+    return produto;
+}
+const produtoTransformado = transformarPreco(nintendo);
+console.log(produtoTransformado);
--- script.ts
+++ script.ts
@@ -1,4 +1,43 @@
-//@ts-check
-const total: number = 100.05
+//Annotation
+let produto: string = "Livro";
+let preco: number = 200;
 
-console.log(total.toFixed())
+const carro: {
+    marca: string;
+    portas: number;
+} = {
+    marca: "Toyota",
+    portas: 5
+}
+
+//Inference
+let produto2 = "Livro";
+let preco2 = 200;
+
+const carro2 = {
+    marca: "Toyota",
+    portas: 5
+}
+
+const barato = preco < 400 ? true : "Produto caro";
+
+//-------
+
+function somar(a: number, b: number) {
+    return a + b;
+}
+
+somar(3, 10);
+
+const nintendo = {
+    nome: "Switch",
+    preco: '2000',
+}
+
+function transformarPreco(produto: { nome: string; preco: string }) {
+    produto.preco = `R$${produto.preco},00`;
+    return produto;
+}
+
+const produtoTransformado = transformarPreco(nintendo);
+console.log(produtoTransformado);
