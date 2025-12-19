df9405ff8a0395d651ae39f3195d87d788ddaab5
oricardos <ricardo.rs95@gmail.com>
2025-12-19 14:51:26 -0300

    exercicios

--- script.js
+++ script.js
@@ -1,31 +1,61 @@
 "use strict";
 //Annotation
-let produto = "Livro";
-let preco = 200;
-const carro = {
-    marca: "Toyota",
-    portas: 5
-};
-//Inference
-let produto2 = "Livro";
-let preco2 = 200;
-const carro2 = {
-    marca: "Toyota",
-    portas: 5
-};
-const barato = preco < 400 ? true : "Produto caro";
-//-------
-function somar(a, b) {
-    return a + b;
+// let produto: string = "Livro";
+// let preco: number = 200;
+// const carro: {
+//     marca: string;
+//     portas: number;
+// } = {
+//     marca: "Toyota",
+//     portas: 5
+// }
+// //Inference
+// let produto2 = "Livro";
+// let preco2 = 200;
+// const carro2 = {
+//     marca: "Toyota",
+//     portas: 5
+// }
+// const barato = preco < 400 ? true : "Produto caro";
+// //-------
+// function somar(a: number, b: number) {
+//     return a + b;
+// }
+// somar(3, 10);
+// const nintendo = {
+//     nome: "Switch",
+//     preco: '2000',
+// }
+// function transformarPreco(produto: { nome: string; preco: string }) {
+//     produto.preco = `R$${produto.preco},00`;
+//     return produto;
+// }
+// const produtoTransformado = transformarPreco(nintendo);
+// console.log(produtoTransformado);
+// EXERCÍCIOS
+//1
+function normalizarTexto(texto) {
+    return texto.trim().toLowerCase();
 }
-somar(3, 10);
-const nintendo = {
-    nome: "Switch",
-    preco: '2000',
-};
-function transformarPreco(produto) {
-    produto.preco = `R$${produto.preco},00`;
-    return produto;
+//2
+const input = document.querySelector('input');
+const total = localStorage.getItem('total');
+if (input && total) {
+    input.value = total;
+    calcularGanho(Number(input.value));
 }
-const produtoTransformado = transformarPreco(nintendo);
-console.log(produtoTransformado);
+function calcularGanho(value) {
+    const p = document.querySelector('p');
+    if (p) {
+        p.innerText = `ganho total: ${value + 100 - value * 0.2}`;
+    }
+}
+function totalMudou() {
+    if (input) {
+        localStorage.setItem('total', input.value);
+        calcularGanho(Number(input.value));
+    }
+}
+if (input) {
+    input.addEventListener('keyup', totalMudou);
+}
--- script.ts
+++ script.ts
@@ -1,43 +1,77 @@
 //Annotation
-let produto: string = "Livro";
-let preco: number = 200;
+// let produto: string = "Livro";
+// let preco: number = 200;
 
-const carro: {
-    marca: string;
-    portas: number;
-} = {
-    marca: "Toyota",
-    portas: 5
+// const carro: {
+//     marca: string;
+//     portas: number;
+// } = {
+//     marca: "Toyota",
+//     portas: 5
+// }
+
+// //Inference
+// let produto2 = "Livro";
+// let preco2 = 200;
+
+// const carro2 = {
+//     marca: "Toyota",
+//     portas: 5
+// }
+
+// const barato = preco < 400 ? true : "Produto caro";
+
+// //-------
+
+// function somar(a: number, b: number) {
+//     return a + b;
+// }
+
+// somar(3, 10);
+
+// const nintendo = {
+//     nome: "Switch",
+//     preco: '2000',
+// }
+
+// function transformarPreco(produto: { nome: string; preco: string }) {
+//     produto.preco = `R$${produto.preco},00`;
+//     return produto;
+// }
+
+// const produtoTransformado = transformarPreco(nintendo);
+// console.log(produtoTransformado);
+
+// EXERCÍCIOS
+//1
+function normalizarTexto(texto: string) {
+    return texto.trim().toLowerCase();
 }
 
-//Inference
-let produto2 = "Livro";
-let preco2 = 200;
+//2
+const input = document.querySelector('input');
 
-const carro2 = {
-    marca: "Toyota",
-    portas: 5
+const total = localStorage.getItem('total');
+if (input && total) {
+    input.value = total;
+    calcularGanho(Number(input.value));
 }
 
-const barato = preco < 400 ? true : "Produto caro";
-
-//-------
-
-function somar(a: number, b: number) {
-    return a + b;
+function calcularGanho(value: number) {
+    const p = document.querySelector('p');
+    if (p) {
+        p.innerText = `ganho total: ${value + 100 - value * 0.2}`;
+    }
 }
 
-somar(3, 10);
-
-const nintendo = {
-    nome: "Switch",
-    preco: '2000',
+function totalMudou() {
+    if (input) {
+        localStorage.setItem('total', input.value);
+        calcularGanho(Number(input.value));
+    }
 }
 
-function transformarPreco(produto: { nome: string; preco: string }) {
-    produto.preco = `R$${produto.preco},00`;
-    return produto;
+if (input) {
+    input.addEventListener('keyup', totalMudou);
 }
 
-const produtoTransformado = transformarPreco(nintendo);
-console.log(produtoTransformado);
