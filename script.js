4dead391db1b442dc26f2809a3bf88231def9652
oricardos <ricardo.rs95@gmail.com>
2025-12-29 10:29:55 -0300

    union types

--- script.js
+++ script.js
@@ -34,28 +34,65 @@
 // console.log(produtoTransformado);
 // EXERCÍCIOS
 //1
-function normalizarTexto(texto) {
-    return texto.trim().toLowerCase();
-}
-//2
-const input = document.querySelector('input');
-const total = localStorage.getItem('total');
-if (input && total) {
-    input.value = total;
-    calcularGanho(Number(input.value));
-}
-function calcularGanho(value) {
-    const p = document.querySelector('p');
-    if (p) {
-        p.innerText = `ganho total: ${value + 100 - value * 0.2}`;
+// function normalizarTexto(texto: string) {
+//     return texto.trim().toLowerCase();
+// }
+// //2
+// const input = document.querySelector('input');
+// const total = localStorage.getItem('total');
+// if (input && total) {
+//     input.value = total;
+//     calcularGanho(Number(input.value));
+// }
+// function calcularGanho(value: number) {
+//     const p = document.querySelector('p');
+//     if (p) {
+//         p.innerText = `ganho total: ${value + 100 - value * 0.2}`;
+//     }
+// }
+// function totalMudou() {
+//     if (input) {
+//         localStorage.setItem('total', input.value);
+//         calcularGanho(Number(input.value));
+//     }
+// }
+// if (input) {
+//     input.addEventListener('keyup', totalMudou);
+// }
+//string, number e boolean
+// const frase: string = 'Front End';
+// const preco: number = 500;
+// const condi: boolean = preco > 100;
+// console.log(typeof frase)
+// console.log(typeof null, `null`)
+// Union Types
+// let total: string | number = 200;
+// total = '300';
+// function isNumber(value: string | number): boolean {
+//     if (typeof value === 'number') {
+//         return true
+//     } else {
+//         return false
+//     }
+// }
+// console.log(isNumber(total))
+//Exercício
+// 1 - Crie uma função chamada toNumber
+// 2 - A função pode receber number | string
+function toNumber(param) {
+    // 3 - Se a função receber um número, retorne um número
+    if (typeof param === 'number') {
+        return param;
+    }
+    else if (typeof param === 'string') {
+        // 4 - Se a função receber uma string, retorne um número
+        return Number(param);
+    }
+    else {
+        // 5 - Se ela receber algo diferente, retorne um erro. (throw "value deve ser um número ou uma string")
+        throw ("param deve ser um número ou uma string");
     }
 }
-function totalMudou() {
-    if (input) {
-        localStorage.setItem('total', input.value);
-        calcularGanho(Number(input.value));
-    }
-}
-if (input) {
-    input.addEventListener('keyup', totalMudou);
-}
+console.log(toNumber(1));
+console.log(toNumber('1'));
+// console.log(toNumber({}))
--- script.ts
+++ script.ts
@@ -44,34 +44,76 @@
 
 // EXERCÍCIOS
 //1
-function normalizarTexto(texto: string) {
-    return texto.trim().toLowerCase();
-}
+// function normalizarTexto(texto: string) {
+//     return texto.trim().toLowerCase();
+// }
 
-//2
-const input = document.querySelector('input');
+// //2
+// const input = document.querySelector('input');
 
-const total = localStorage.getItem('total');
-if (input && total) {
-    input.value = total;
-    calcularGanho(Number(input.value));
-}
+// const total = localStorage.getItem('total');
+// if (input && total) {
+//     input.value = total;
+//     calcularGanho(Number(input.value));
+// }
 
-function calcularGanho(value: number) {
-    const p = document.querySelector('p');
-    if (p) {
-        p.innerText = `ganho total: ${value + 100 - value * 0.2}`;
+// function calcularGanho(value: number) {
+//     const p = document.querySelector('p');
+//     if (p) {
+//         p.innerText = `ganho total: ${value + 100 - value * 0.2}`;
+//     }
+// }
+
+// function totalMudou() {
+//     if (input) {
+//         localStorage.setItem('total', input.value);
+//         calcularGanho(Number(input.value));
+//     }
+// }
+
+// if (input) {
+//     input.addEventListener('keyup', totalMudou);
+// }
+
+//string, number e boolean
+// const frase: string = 'Front End';
+// const preco: number = 500;
+// const condi: boolean = preco > 100;
+
+// console.log(typeof frase)
+// console.log(typeof null, `null`)
+
+// Union Types
+// let total: string | number = 200;
+// total = '300';
+
+// function isNumber(value: string | number): boolean {
+//     if (typeof value === 'number') {
+//         return true
+//     } else {
+//         return false
+//     }
+// }
+
+// console.log(isNumber(total))
+
+
+//Exercício
+// 1 - Crie uma função chamada toNumber
+// 2 - A função pode receber number | string
+function toNumber(param: number | string): number | string {
+    // 3 - Se a função receber um número, retorne um número
+    if (typeof param === 'number') {
+        return param
+    } else if (typeof param === 'string') {
+        // 4 - Se a função receber uma string, retorne um número
+        return Number(param)
+    } else {
+        // 5 - Se ela receber algo diferente, retorne um erro. (throw "value deve ser um número ou uma string")
+        throw ("param deve ser um número ou uma string")
     }
 }
 
-function totalMudou() {
-    if (input) {
-        localStorage.setItem('total', input.value);
-        calcularGanho(Number(input.value));
-    }
-}
-
-if (input) {
-    input.addEventListener('keyup', totalMudou);
-}
-
+console.log(toNumber(1))
+console.log(toNumber('1'))
+// console.log(toNumber({}))
