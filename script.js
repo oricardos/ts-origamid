b8ee658e1316b4405f71cfd39195f6c2d5c8cad7
oricardos <ricardo.rs95@gmail.com>
2025-12-30 15:01:21 -0300

    tuplas

--- script.js
+++ script.js
@@ -666,33 +666,59 @@
 // const form = document.querySelector<HTMLElement>('#form');
 // form?.addEventListener('keyup', handleInput);
 // classes
-class Produto {
-    // public: é o padrão de qualquer uma
-    // private: só pode ser acessada dentro da classe
-    tipo = 'produto';
-    // protected: só pode ser acessada dentro da classe e subclasses
-    preco;
-    // readonly: só permite leitura
-    nome;
-    constructor(nome, preco) {
-        this.nome = nome;
-        this.tipo;
-        this.preco = preco;
-    }
-    getTipo() {
-        return this.tipo;
-    }
-    getPreco() {
-        return Produto.transformarPreco(this.preco);
-    }
-    // [javascript] static: não fará parte do Objeto criado
-    // e sim da função construtora (classe)
-    static transformarPreco(preco) {
-        return `R$ ${preco}`;
-    }
-}
-const livro = new Produto('O Senhor dos Aneis', 200);
-console.log(livro.getTipo());
-console.log(livro.getPreco());
-console.log(livro.nome);
-console.log(Produto.transformarPreco(100));
+// class Produto {
+//     // public: é o padrão de qualquer uma
+//     // private: só pode ser acessada dentro da classe
+//     private tipo = 'produto';
+//     // protected: só pode ser acessada dentro da classe e subclasses
+//     protected preco: number;
+//     // readonly: só permite leitura
+//     readonly nome: string;
+//     constructor(nome: string, preco: number) {
+//         this.nome = nome;
+//         this.tipo;
+//         this.preco = preco;
+//     }
+//     getTipo() {
+//         return this.tipo;
+//     }
+//     getPreco() {
+//         return Produto.transformarPreco(this.preco);
+//     }
+//     // [javascript] static: não fará parte do Objeto criado
+//     // e sim da função construtora (classe)
+//     static transformarPreco(preco: number) {
+//         return `R$ ${preco}`;
+//     }
+// }
+// const livro = new Produto('O Senhor dos Aneis', 200);
+// console.log(livro.getTipo());
+// console.log(livro.getPreco());
+// console.log(livro.nome);
+// console.log(Produto.transformarPreco(100));
+// Tuplas
+//Tuples são arrays que possuem dados em posições fixas.
+// const produto1: (string | number)[] = ['Notebook', 2500];
+// const produto2: [string, number] = ['Notebook', 2500];
+// const nome1 = produto1[0]; // string | number
+// const nome2 = produto2[0]; // string
+// const [nome, preco] = produto2;
+// //Exercicio
+// async function fetchVendas() {
+//     const response = await fetch('https://api.origamid.dev/json/vendas.json')
+//     const data = await response.json()
+//     somarVendas(data)
+// }
+// fetchVendas()
+// interface ProdutoDetalhes {
+//     marca: string;
+//     cor: string;
+// }
+// type Venda = [string, number, string, ProdutoDetalhes]
+// function somarVendas(vendas: Venda[]) {
+//     let total = 0
+//     for (let i = 0; i < vendas.length; i++) {
+//         total += vendas[i][1]
+//     }
+//     document.body.innerHTML += `Total: <p>${total}</p>`
+// }
