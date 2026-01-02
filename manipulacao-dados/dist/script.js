aa62066c6713fa2ade9014712e90507d3e83982e
oricardos <ricardo.rs95@gmail.com>
2026-01-02 14:06:08 -0300

    best day

--- manipulacao-dados/dist/script.js
+++ manipulacao-dados/dist/script.js
@@ -35,6 +35,48 @@ function fillStatistics(transacoes) {
     fillList(totalPagamentoValue, 'pagamento');
     const totalStatusValue = countBy(transacoes.map(({ status }) => status));
     fillList(totalStatusValue, 'status');
+    const semana = {
+        ['Domingo']: 0,
+        ['Segunda']: 1,
+        ['Terça']: 2,
+        ['Quarta']: 3,
+        ['Quinta']: 4,
+        ['Sexta']: 5,
+        ['Sábado']: 6,
+    };
+    for (let i = 0; i < transacoes.length; i++) {
+        const day = transacoes[i].data.getDay();
+        switch (day) {
+            case 0:
+                semana['Domingo'] += 1;
+                break;
+            case 1:
+                semana['Segunda'] += 1;
+                break;
+            case 2:
+                semana['Terça'] += 1;
+                break;
+            case 3:
+                semana['Quarta'] += 1;
+                break;
+            case 4:
+                semana['Quinta'] += 1;
+                break;
+            case 5:
+                semana['Sexta'] += 1;
+                break;
+            case 6:
+                semana['Sábado'] += 1;
+                break;
+        }
+    }
+    const vendaValue = Object.entries(semana).sort((a, b) => {
+        return b[1] - a[1];
+    })[0];
+    const vendaElement = document.querySelector('#venda span');
+    if (vendaElement) {
+        vendaElement.innerText = vendaValue[0];
+    }
 }
 function fillTable(transacoes) {
     const table = document.querySelector('#transacoes tbody');
--- manipulacao-dados/src/script.ts
+++ manipulacao-dados/src/script.ts
@@ -43,6 +43,58 @@ function fillStatistics(transacoes: Transacao[]): void {
 
     const totalStatusValue = countBy(transacoes.map(({ status }) => status))
     fillList(totalStatusValue, 'status')
+
+    const semana = {
+        ['Domingo']: 0,
+        ['Segunda']: 1,
+        ['Terça']: 2,
+        ['Quarta']: 3,
+        ['Quinta']: 4,
+        ['Sexta']: 5,
+        ['Sábado']: 6,
+    }
+    for (let i = 0; i < transacoes.length; i++) {
+        const day = transacoes[i].data.getDay()
+        switch (day) {
+            case 0:
+                semana['Domingo'] += 1
+                break
+
+            case 1:
+                semana['Segunda'] += 1
+                break
+
+            case 2:
+                semana['Terça'] += 1
+                break
+
+            case 3:
+                semana['Quarta'] += 1
+                break
+
+            case 4:
+                semana['Quinta'] += 1
+                break
+
+            case 5:
+                semana['Sexta'] += 1
+                break
+
+            case 6:
+                semana['Sábado'] += 1
+                break
+        }
+    }
+
+    const vendaValue = Object.entries(semana).sort((a, b) => {
+        return b[1] - a[1]
+    })[0]
+
+    const vendaElement = document.querySelector<HTMLElement>('#venda span')
+    if (vendaElement) {
+        vendaElement.innerText = vendaValue[0]
+    }
+
 }
 
 function fillTable(transacoes: Transacao[]): void {
