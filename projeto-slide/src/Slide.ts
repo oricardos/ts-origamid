08e813d40ef7f00f36ec01143e3db3789a9b2bd6
oricardos <ricardo.rs95@gmail.com>
2026-01-02 16:07:59 -0300

    timeout pause

--- projeto-slide/src/Slide.ts
+++ projeto-slide/src/Slide.ts
@@ -8,6 +8,8 @@ export default class Slide {
     index;
     slide;
     timeout: Timeout | null;
+    pausedTimeout: Timeout | null;
+    paused;
     constructor(
         container: Element,
         slides: Element[],
@@ -20,8 +22,10 @@ export default class Slide {
         this.time = time;
 
         this.timeout = null
+        this.pausedTimeout = null
         this.index = 0;
         this.slide = this.slides[this.index]
+        this.paused = false;
 
         this.init()
     }
@@ -46,13 +50,29 @@ export default class Slide {
     }
 
     prev() {
+        if (this.paused) return;
         const prev = this.index > 0 ? this.index - 1 : this.slides.length - 1;
         this.show(prev)
     }
 
     next() {
+        if (this.paused) return;
         const next = this.index + 1 < this.slides.length ? this.index + 1 : 0
         this.show(next)
+    }
+
+    pause() {
+        this.pausedTimeout = new Timeout(() => {
+            this.paused = true
+        }, 300)
+    }
+
+    continue() {
+        this.pausedTimeout?.clear()
+        if (this.paused) {
+            this.paused = false
+            this.auto(this.time)
+        }
     }
 
     private addControls() {
@@ -62,6 +82,9 @@ export default class Slide {
         nextButton.innerText = "Próximo";
         this.controls.appendChild(prevButton)
         this.controls.appendChild(nextButton)
+
+        this.controls.addEventListener("pointerdown", () => this.pause())
+        this.controls.addEventListener("pointerup", () => this.continue())
 
         prevButton.addEventListener('pointerup', () => this.prev())
         nextButton.addEventListener('pointerup', () => this.next())
